21ec1c8b0f55dbf01288c7d15ff0e464bf1dd48c
ludvi <ludvi@DESKTOP-6U5E7KR>
2020-06-04 15:22:19 +0200

    added dbsets to dbcontext

--- Lexicon_LMS/Data/ApplicationDbContext.cs
+++ Lexicon_LMS/Data/ApplicationDbContext.cs
@@ -1,16 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Lexicon_LMS.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Lexicon_LMS.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<User,IdentityRole,string>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+
         }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Module> Modules { get; set; }
+        public DbSet<Activity> Activities { get; set; }
+        public DbSet<Document> Documents { get; set; }
+        public DbSet<ActivityType> ActivityTypes { get; set; }
     }
 }
