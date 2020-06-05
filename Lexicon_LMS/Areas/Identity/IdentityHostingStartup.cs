c33f76309ec3810d2ae9925dd3d6cf67e4bf1730
ludvi <ludvi@DESKTOP-6U5E7KR>
2020-06-05 10:44:27 +0200

    Scafolded login,register and logout pages

---
+++ Lexicon_LMS/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Lexicon_LMS.Data;
+using Lexicon_LMS.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Lexicon_LMS.Areas.Identity.IdentityHostingStartup))]
+namespace Lexicon_LMS.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
