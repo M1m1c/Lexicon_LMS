07d4c2f6db99577ba5428e4ff335fb67552f2dde
JoJo <unslavedcoder@gmail.com>
2020-06-08 14:04:41 +0200

    correction of models

--- Lexicon_LMS/Models/Course.cs
+++ Lexicon_LMS/Models/Course.cs
@@ -9,14 +9,9 @@ namespace Lexicon_LMS.Models
     public class Course
     {
         public int Id { get; set; }
-
-        [Display(Name = "Course")]
         public string CourseName { get; set; }
         public string Description { get; set; }
-
-        [Display(Name = "Starting date")]
         public DateTime StartDate { get; set; }
-
         public ICollection<Module> Modules { get; set; }
         public ICollection<User> Users { get; set; }
         public ICollection<Document> Documents { get; set; }
