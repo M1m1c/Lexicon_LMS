eb01022f3f10c21fc5dce4baf57d5ba62ac26ba3
ludvi <ludvi@DESKTOP-6U5E7KR>
2020-06-08 15:31:52 +0200

    Added Eidt ser View model

---
+++ Lexicon_LMS/Models/ViewModels/EditUserViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lexicon_LMS.Models.ViewModels
+{
+    public class EditUserViewModel
+    {
+        [Key]
+        public string Id { get; set; }
+
+        [Required]
+        public int Age { get; set; }
+
+        [Required]
+        public String FirstName { get; set; }
+
+        [Required]
+        public String LastName { get; set; }
+
+        [Required]
+        public string Role { get; set; }
+
+    }
+}
