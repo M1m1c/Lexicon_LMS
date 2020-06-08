711500498894727ceb05a9d23accebc6adbabdfb
ludvi <ludvi@DESKTOP-6U5E7KR>
2020-06-08 13:31:41 +0200

    Added password conirmation to create user and user viewmodel

--- Lexicon_LMS/Models/ViewModels/UserViewModel.cs
+++ Lexicon_LMS/Models/ViewModels/UserViewModel.cs
@@ -28,6 +28,11 @@ namespace Lexicon_LMS.Models.ViewModels
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
         [Required]
         public string Role { get; set; }
     }
