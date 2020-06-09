a8f17bed86f77e44c3cbcc2b16884e77424b3899
ludvi <ludvi@DESKTOP-6U5E7KR>
2020-06-09 11:34:20 +0200

    Added ModuleViewModel

---
+++ Lexicon_LMS/Models/ViewModels/ModuleViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lexicon_LMS.Models.ViewModels
+{
+    public class ModuleViewModel
+    {
+
+        public int Id { get; set; }
+        public string ModuleName { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+
+        public int CourseId { get; set; }
+
+        public Course Course { get; set; }
+        public ICollection<Document> Documents { get; set; }
+        public ICollection<CourseActivity> Activities { get; set; }
+    }
+}
