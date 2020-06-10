a6c7169a3e49d8bd39880f7ba4627ef2b154438c
ludvi <ludvi@DESKTOP-6U5E7KR>
2020-06-10 14:15:36 +0200

    Fixed so that teacher login redirects to teacher start partial

--- Lexicon_LMS/Controllers/UserController.cs
+++ Lexicon_LMS/Controllers/UserController.cs
@@ -2,12 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Lexicon_LMS.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Lexicon_LMS.Controllers
 {
     public class UserController : Controller
     {
+        private readonly UserManager<User> userManager;
+
+        public UserController(UserManager<User> userManager)
+        {
+            this.userManager = userManager;
+        }
         public IActionResult Index()
         {
             return View();
@@ -15,6 +23,12 @@ namespace Lexicon_LMS.Controllers
 
         public IActionResult Start()
         {
+            
+            if (User.IsInRole("Teacher"))
+            {
+                return RedirectToAction("TeacherStartPartial", "Teacher");
+            }
+
             return View();
         }
     }
