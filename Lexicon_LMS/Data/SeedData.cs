7e0b213c6c30ac1662c1bd92eba9e87bea31092d
ludvi <ludvi@DESKTOP-6U5E7KR>
2020-06-11 11:35:05 +0200

    Added Seed Activity Types and refactored seed data

--- Lexicon_LMS/Data/SeedData.cs
+++ Lexicon_LMS/Data/SeedData.cs
@@ -22,45 +22,75 @@ namespace Lexicon_LMS.Data
 
                 var roleNames = new[] { "Teacher", "Student" };
 
-                foreach (var name in roleNames)
-                {
-                    if (await roleManager.RoleExistsAsync(name)) continue;
+                await SeedRoles(roleManager, roleNames);
 
-                    var role = new IdentityRole { Name = name };
-                    var result = await roleManager.CreateAsync(role);
-
-                    if (!result.Succeeded) throw new Exception(string.Join("\n", result.Errors));
-                }
                 var teacherEmail = "teacher@lexicon.se";
+                var teacherPassword= "Abc12#";
 
-                var foundUser = await userManager.FindByEmailAsync(teacherEmail);
+                await SeedTeacher(userManager, roleManager, roleNames, teacherEmail, teacherPassword);
 
-                if (foundUser != null) return;
-
-                var user = new User
-                {
-                    UserName = teacherEmail,
-                    Email = teacherEmail
-
-                };
-
-                var addUserResult = await userManager.CreateAsync(user,"Abc12#");
-
-                if (!addUserResult.Succeeded) throw new Exception(string.Join("\n", addUserResult.Errors));
-
-
-                var teacherUser = await userManager.FindByNameAsync(teacherEmail);
-
-                foreach (var role in roleNames)
-                {
-                    if (await userManager.IsInRoleAsync(teacherUser, role)) continue;
-
-                    var addToRoleResult = await userManager.AddToRoleAsync(teacherUser, role);
-
-                    if (!addToRoleResult.Succeeded) throw new Exception(string.Join("\n", addToRoleResult.Errors));
-                }
-
+                await SeedActiityTypes(context);
             }
+        }
+
+        private static async Task SeedRoles(RoleManager<IdentityRole> roleManager, string[] roleNames)
+        {
+            foreach (var name in roleNames)
+            {
+                if (await roleManager.RoleExistsAsync(name)) continue;
+
+                var role = new IdentityRole { Name = name };
+                var result = await roleManager.CreateAsync(role);
+
+                if (!result.Succeeded) throw new Exception(string.Join("\n", result.Errors));
+            }
+        }
+
+        private static async Task SeedTeacher(UserManager<User> userManager, RoleManager<IdentityRole> roleManager, string[] roleNames,string teacherEmail,string teacherPassword)
+        {
+            var foundUser = await userManager.FindByEmailAsync(teacherEmail);
+
+            if (foundUser != null) return;
+
+            var user = new User
+            {
+                UserName = teacherEmail,
+                Email = teacherEmail
+
+            };
+
+            var addUserResult = await userManager.CreateAsync(user, teacherPassword);
+
+            if (!addUserResult.Succeeded) throw new Exception(string.Join("\n", addUserResult.Errors));
+
+
+            var teacherUser = await userManager.FindByNameAsync(teacherEmail);
+
+            foreach (var role in roleNames)
+            {
+                if (await userManager.IsInRoleAsync(teacherUser, role)) continue;
+
+                var addToRoleResult = await userManager.AddToRoleAsync(teacherUser, role);
+
+                if (!addToRoleResult.Succeeded) throw new Exception(string.Join("\n", addToRoleResult.Errors));
+            }
+        }
+
+        private static async Task SeedActiityTypes(ApplicationDbContext context)
+        {
+            if (context.ActivityTypes.Any())
+            {
+                return;
+            }
+
+            context.ActivityTypes.AddRange(
+                new ActivityType { Name = "Assignment" },
+                new ActivityType { Name = "E-Learning" },
+                new ActivityType { Name = "Lecture" },
+                new ActivityType { Name = "Exam" }
+                );
+
+            await context.SaveChangesAsync();
         }
     }
 }
