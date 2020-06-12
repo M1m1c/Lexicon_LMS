9d681d9a290f66a3a9ae25a7ab61e74679740135
ludvi <ludvi@DESKTOP-6U5E7KR>
2020-06-12 16:48:47 +0200

    Added document view model

---
+++ Lexicon_LMS/Models/ViewModels/DocumentViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Lexicon_LMS.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lexicon_LMS.Models.ViewModels
+{
+    public class DocumentViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime UploadDate { get; set; }
+
+        public string UserId { get; set; }
+
+        public int? HolderId { get; set; }
+        public HolderTypeEnum HolderType { get; set; }
+
+        public User User { get; set; }
+    }
+
+  
+}
